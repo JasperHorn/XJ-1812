4a1b90a7b5c9bb8c96f7bd4042ca248f27fd9f81
Jasper Horn <jasperhorn@gmail.com>
2019-01-12 17:30:14 +0100

    Finally putting this thing into git

---
+++ bot.js
@@ -0,0 +1,301 @@
+var Discord = require('discord.js');
+var auth = require('./auth.json');
+
+// Initialize Discord Bot
+var bot = new Discord.Client();
+
+bot.on('ready', function () {
+    console.log('Connected');
+    console.log('Logged in as: ');
+    console.log(bot.user.username + ' - (' + bot.user.tag + ')');
+    
+    bot.user.setActivity('/xj-1812');
+});
+
+bot.on('message', function (message) {
+    if (message.content.substring(0, 1) == '/') {
+        var args = message.content.substring(1).split(' ');
+        var cmd = args[0];
+        
+        switch(cmd) {
+            case 'xj-1812':
+                help(message, args);
+                break;
+            case 'slap':
+                slap(message, args);
+                break;
+            case 'countdown':
+                countdown(message, args);
+                break;
+            case 'deletethis':
+                deleteThis(message, args);
+                break;
+            case 'selfdeletingmessage':
+                selfDeletingMessage(message, args);
+                break;
+            case 'selfdestructingmessage':
+                selfDestructingMessage(message, args);
+            case 'roll':
+                roll(message, args);
+                break;
+            case 'randomperson':
+                randomPerson(message, args);
+                break;
+         }
+     }
+});
+
+function delay(time, value) {
+   return new Promise(function(resolve) { 
+       setTimeout(resolve.bind(null, value), time)
+   });
+}
+
+function nickname(guild, user) {
+    var guildName = null;
+    
+    if (guild) {
+        guildName = guild.member(user).nickname;
+    }
+    
+    if (guildName !== null) {
+        return guildName;
+    }
+    else {
+        return user.username;
+    }
+}
+
+function authorNickname(message) {
+    return nickname(message.guild, message.author);
+}
+
+function help(message, args) {
+    var response = 'Hi! My name is XJ-1812. I do things when you begin your message with one of the following: \n\
+    /xj-1812 \n\
+    /slap \n\
+    /countdown \n\
+    /deletethis \n\
+    /selfdeletingmessage \n\
+    /selfdestructingmessage \n\
+    /roll <n>d<x> \n\
+    /randomperson [<status|role|allowbots>[|...]] \n\
+    Feel free to experiment!';
+    
+    message.channel.send(response);
+}
+
+function slap(message, args) {    
+    var response = '*' + authorNickname(message) + ' ';
+    response += 'slaps'
+    
+    for (var i = 1; i < args.length; i++) {
+        response += ' ' + args[i];
+    }
+    
+    response += ' around a bit with a large trout*'
+    
+    message.channel.send(response);
+}
+
+function countdown(message, args) {
+    var timeout;
+    
+    if (args.length > 1) {
+        timeout = parseInt(args[1]);
+    }
+    else {
+        timeout = 5;
+    }
+    
+    promisedCountdown(timeout, message.channel).then(function (countdownMessage) {
+        countdownMessage.edit("Lift-off!");
+    });
+}
+
+function promisedCountdown(startingAt, channel) {
+    var timeout = startingAt;
+    
+    function updateMessage(ownMessage) {
+        if (timeout > 0) {
+            ownMessage.edit(getCurrentCount());
+            return [true, ownMessage];
+        }
+        else {
+            return [false, ownMessage];
+        }
+    }
+    
+    function getCurrentCount() {
+        if (timeout > 10) {
+            return Math.floor(timeout/ 10).toString() + "~";
+        }
+        else {
+            return timeout.toString();
+        }
+        
+        return ownMessage;
+    }
+    
+    function updateLoop(ownMessage) {
+        var delta;
+        
+        if (timeout <= 10) {
+            delta = 1;
+        }
+        else {
+            if (timeout % 10 == 9) {
+                delta = 10;
+            }
+            else {
+                delta = (timeout % 10) + 1;
+            }
+            
+            if (timeout < 20) {
+                delta--;
+            }
+        }
+        
+        timeout -= delta;
+        
+        return delay(1000 * delta, ownMessage).then(updateMessage).then(function (promiseArguments) {
+            if (!promiseArguments[0]) {
+                return promiseArguments[1];
+            }
+            else {
+                return updateLoop(promiseArguments[1]);
+            }
+        });
+    }
+    
+    return channel.send(getCurrentCount()).then(updateLoop);
+}
+
+function deleteThis(message, args) {
+    message.delete();
+}
+
+function selfDeletingMessage(message, args) {
+    delay(10000).then(function () {
+        message.delete();
+    });
+}
+
+function selfDestructingMessage(message, args) {
+    var secretMessageText = message.content.replace('/selfdestructingmessage', authorNickname(message) + ':');
+    message.delete();
+    
+    var secretMessage;
+    
+    message.channel.send(secretMessageText).then(function (ownMessage) {
+        secretMessage = ownMessage;
+        return promisedCountdown(10, message.channel);
+    }).then(function (countdownMessage) {
+        countdownMessage.delete();
+        secretMessage.edit("Kaboom!");
+    });
+}
+
+function roll(message, args) {
+    var dice = args[1].split('d', 2);
+    
+    var count = parseInt(dice[0]);
+    var dieType = parseInt(dice[1]);
+    
+    if (count <= 0) {
+        count = 1;
+    }
+    
+    if (count > 20) {
+        count = 20;
+    }
+    
+    if (dieType <= 0) {
+        dieType = 6;
+    }
+    
+    function dieRoll(sides) {
+        return randomInteger(1, sides);
+    }
+    
+    var results = [];
+    for (var i = 0; i < count; i++) {
+        results.push(dieRoll(dieType));
+    }
+    
+    var output = "Rolled " + count.toString() + "d" + dieType.toString() + ": ";
+    output += results.reduce((a, b) => a + b, 0);
+    
+    output += ' (';
+    output += results.reduce((a, b) => a.toString() + ", " + b.toString());
+    output += ')';
+    
+    message.channel.send(output);
+}
+
+function randomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomPerson(message, args) {
+    var guild = message.channel.guild;
+    
+    if (!guild) {
+        message.channel.send("I pick you! Wait, there's nobody else to pick from, because you aren't asking me on a server...");
+        return;
+    }
+    
+    var filter = {
+        statuses: [],
+        roles: [],
+        allowBots: false
+    };
+    
+    var allStatuses = ['online', 'offline', 'idle', 'dnd'];
+    var allRoles = guild.roles.map(role => role.name.toLowerCase());
+    
+    if (args.length > 1) {
+        var rawFilter = args[1].split('|');
+        
+        rawFilter.forEach(function (singleFilter) {
+            if (allStatuses.includes(singleFilter)) {
+                filter.statuses.push(singleFilter);
+            }
+            else if (singleFilter == 'allowbots') {
+                filter.allowBots = true;
+            }
+            else if (allRoles.includes(singleFilter.toLowerCase())) {
+                filter.roles.push(singleFilter.toLowerCase());
+            }
+            else {
+                message.channel.send('I do not understand what ' + singleFilter + " means, so I'm skipping it");
+            }
+        });
+    }
+    
+    var people = guild.members.array();
+    
+    if (!filter.allowBots) {
+        people = people.filter(member => !member.user.bot);
+    }
+    
+    if (filter.statuses.length > 0) {
+        people = people.filter(member => filter.statuses.includes(member.user.presence.status));
+    }
+    
+    if (filter.roles.length > 0) {
+        people = people.filter(member => member.roles.some(role => filter.roles.includes(role.name.toLowerCase())));
+    }
+    
+    if (people.length == 0) {
+        message.channel.send("Nobody to pick from!");
+        return;
+    }
+    
+    var personIndex = randomInteger(0, people.length - 1);
+    var person = people[personIndex];
+    
+    message.channel.send("Randomly picked: " + nickname(guild, person.user));
+}
+
+bot.login(auth.token);
