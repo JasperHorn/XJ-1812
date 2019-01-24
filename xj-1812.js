e67014f8220a6f6ef03d04a21c4ceb6a59574dd2
Jasper Horn <jasperhorn@gmail.com>
2019-01-24 19:45:34 +0100

    Reorder modules
    Really just changes the order things are listed in for the help command.

--- xj-1812.js
+++ xj-1812.js
@@ -3,15 +3,15 @@ var Bot = require('./bot.js');
 
 var commandModules = [];
 
-commandModules.push(require('./command-modules/sqlpoc.js'));
+commandModules.push(require('./command-modules/slap.js'));
 commandModules.push(require('./command-modules/store-image.js'));
 commandModules.push(require('./command-modules/booyeah.js'));
-commandModules.push(require('./command-modules/slap.js'));
-commandModules.push(require('./command-modules/self-destroy.js'));
-commandModules.push(require('./command-modules/dice.js'));
-commandModules.push(require('./command-modules/lottery.js'));
 commandModules.push(require('./command-modules/secret-messages.js'));
+commandModules.push(require('./command-modules/lottery.js'));
+commandModules.push(require('./command-modules/dice.js'));
+commandModules.push(require('./command-modules/self-destroy.js'));
 commandModules.push(require('./command-modules/save-attachments.js'));
+commandModules.push(require('./command-modules/sqlpoc.js'));
 
 var config = require('./config.json');
 
